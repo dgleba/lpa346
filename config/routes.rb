76421b94e9dc697c6ed588ebaaf91d5aea49a5b6
albe <albe@pmdsdata9.local>
2017-05-03 15:45:43 -0400

    scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
  
 
+  resources :products
+  resources :product_features
+  resources :pfeatures
+  resources :country_of_origins
   mount RailsAdmin::Engine => '/radmin', as: 'rails_admin'
   resources :roles
   devise_for :users, controllers: { sessions: 'users/sessions' }
