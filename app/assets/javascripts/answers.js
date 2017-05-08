f8ec62740327342f8fa232b9a69d23623eba340f
dgleba <dgleba@gmail.com>
2017-05-08 13:06:55 -0400

    Add JS confirmation popup to allow user to stay on page if there is unsaved form data.

---
+++ app/assets/javascripts/answers.js
@@ -0,0 +1,74 @@
+//
+//  asks before leaving page of edited/unsaved form..
+//
+//  \\10.4.1.227\rt\srv\share\lpa346\app\assets\javascripts\answers.js
+//
+// https://blog.driftingruby.com/form-check-modified/
+// others..
+// http://stackoverflow.com/questions/16322042/jquery-warn-if-leaving-page-without-clicking-the-save-button
+// http://stackoverflow.com/questions/10591378/confirm-leave-page-unless-they-clicked-the-save-button
+//
+
+var msg, unsaved;
+ 
+msg = "Changes you made may not be saved.";
+ 
+unsaved = false;
+ 
+$(document).on('change', 'form[role="check-unsaved-form1"]:not([data-remote]) :input', function() {
+  return unsaved = true;
+});
+ 
+$(document).on('turbolinks:load', function() {
+  return unsaved = false;
+});
+ 
+$(document).on('submit', 'form[role="check-unsaved-form1"]', function() {
+  unsaved = false;
+});
+ 
+$(window).bind('beforeunload', function() {
+  if (unsaved) {
+    return msg;
+  }
+});
+ 
+$(document).on('turbolinks:before-visit', function(event) {
+  if (unsaved && !confirm(msg)) {
+    return event.preventDefault();
+  }
+});
+
+
+//
+// tests..
+//
+
+/*
+Add this to view template..
+<br>
+<hr>
+JS: test to see js is working for me.
+<button onclick="myFunction()"> Tryme:  popup javascript alert for a test</button>
+<p class="tryme2"> Tryme2 </p>
+*/
+
+// works.. pops up when tryme2 is clicked..
+$(document).ready(function(){
+  $('.tryme2').click(function(){
+    alert("You clicked: " + $(this).data('id'));
+  });
+});
+
+/*
+// works.. it pops up tryme3 on page load.
+$(document).ready(function(){
+    alert("tryme3 ");
+});
+*/
+
+// works.. pops up when tryme button is clicked.
+function myFunction() {
+    alert("I am an alert box!");
+}
+
