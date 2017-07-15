22ef5a8c141c949b7711ad601c0a27536e49384e
albe <albe@>
2017-07-15 12:56:31 -0400

    schema-dump

--- db/schema.rb
+++ db/schema.rb
@@ -12,21 +12,21 @@
 
 ActiveRecord::Schema.define(version: 50161230223306) do
 
-  create_table "answers", force: :cascade do |t|
-    t.text     "body"
+  create_table "answers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "body",              limit: 65535
     t.integer  "user_id"
     t.integer  "question_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.text     "issue_description"
-    t.text     "action_taken"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.text     "issue_description", limit: 65535
+    t.text     "action_taken",      limit: 65535
     t.integer  "survey_id"
-    t.index ["question_id"], name: "index_answers_on_question_id"
-    t.index ["survey_id"], name: "index_answers_on_survey_id"
-    t.index ["user_id"], name: "index_answers_on_user_id"
+    t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
+    t.index ["survey_id"], name: "index_answers_on_survey_id", using: :btree
+    t.index ["user_id"], name: "index_answers_on_user_id", using: :btree
   end
 
-  create_table "country_of_origins", force: :cascade do |t|
+  create_table "country_of_origins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "fdate"
     t.integer  "active_status"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 50161230223306) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "part_numbers", force: :cascade do |t|
+  create_table "part_numbers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.integer  "sort"
     t.integer  "active_status"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 50161230223306) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "pfeatures", force: :cascade do |t|
+  create_table "pfeatures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "fdate"
     t.integer  "active_status"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 50161230223306) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "process_steps", force: :cascade do |t|
+  create_table "process_steps", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.integer  "sort"
     t.integer  "active_status"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 50161230223306) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "product_features", force: :cascade do |t|
+  create_table "product_features", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.integer  "product_id"
     t.integer  "pfeature_id"
@@ -68,11 +68,11 @@ ActiveRecord::Schema.define(version: 50161230223306) do
     t.integer  "sort"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["pfeature_id"], name: "index_product_features_on_pfeature_id"
-    t.index ["product_id"], name: "index_product_features_on_product_id"
+    t.index ["pfeature_id"], name: "index_product_features_on_pfeature_id", using: :btree
+    t.index ["product_id"], name: "index_product_features_on_product_id", using: :btree
   end
 
-  create_table "products", force: :cascade do |t|
+  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.integer  "country_of_origin_id"
     t.datetime "pdate"
@@ -80,34 +80,34 @@ ActiveRecord::Schema.define(version: 50161230223306) do
     t.integer  "sort"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
-    t.index ["country_of_origin_id"], name: "index_products_on_country_of_origin_id"
+    t.index ["country_of_origin_id"], name: "index_products_on_country_of_origin_id", using: :btree
   end
 
-  create_table "question_lists", force: :cascade do |t|
+  create_table "question_lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.text     "partnumberguide"
+    t.text     "partnumberguide", limit: 65535
     t.integer  "sort"
     t.integer  "active_status"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
-  create_table "questions", force: :cascade do |t|
+  create_table "questions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title_notused"
     t.integer  "survey_id"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.integer  "qtype",                default: 0
-    t.text     "question_note"
-    t.text     "question_whatlookfor"
-    t.text     "question_if_no"
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.integer  "qtype",                              default: 0
+    t.text     "question_note",        limit: 65535
+    t.text     "question_whatlookfor", limit: 65535
+    t.text     "question_if_no",       limit: 65535
     t.integer  "question_list_id"
-    t.integer  "active_status",        default: 0
-    t.integer  "sort",                 default: 0
-    t.index ["question_list_id"], name: "index_questions_on_question_list_id"
+    t.integer  "active_status",                      default: 0
+    t.integer  "sort",                               default: 0
+    t.index ["question_list_id"], name: "index_questions_on_question_list_id", using: :btree
   end
 
-  create_table "roles", force: :cascade do |t|
+  create_table "roles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "description"
     t.integer  "active_status"
@@ -116,7 +116,7 @@ ActiveRecord::Schema.define(version: 50161230223306) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "surveys", force: :cascade do |t|
+  create_table "surveys", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
@@ -125,7 +125,7 @@ ActiveRecord::Schema.define(version: 50161230223306) do
     t.integer  "sort",          default: 0
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -140,9 +140,9 @@ ActiveRecord::Schema.define(version: 50161230223306) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.integer  "role_id",                default: 7
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["role_id"], name: "index_users_on_role_id"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["role_id"], name: "index_users_on_role_id", using: :btree
   end
 
 end
