fde8fec7cec40caeb424dc22b00ea92754dea745
David Gleba <dgleba@gmail.com>
2017-07-19 17:28:14 +0000

    Working yesno validation at KW Ruby

---
+++ _offline/notes-2017-07-18.rb
@@ -0,0 +1,11 @@
+add class to yes radio button
+
+jquery handler
+
+if click yes, hide text fields and they will not submit with the form. it will all be ok. they will be nil.
+    
+    if no, show the fields, make them required by jquery find parent or next or something.
+        
+        use class not id. class can have same name on many objects. id should be unique on the dom.
+        
+        2017-07-18
